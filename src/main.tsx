0df247266c19b4f968e4e7f4408ba94773b79288
PHILBRO <philip@clawdia.ai>
2024-08-15 13:11:35 +0300

    js app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { CONFIG } from "./config/index.ts";
 import "./infra/index.ts";
-// import App from "./app/App.tsx";
-// import { Provider } from "react-redux";
-// import { store } from "./infra/index.ts";
+import App from "./app/App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./infra/index.ts";
 import { mixPanelApi } from "./lib/mixpanel/index.ts";
 import { SentryApi } from "./lib/sentry/index.ts";
 import "./index.css";
@@ -13,8 +13,8 @@ console.log("CONFIG", CONFIG);
 mixPanelApi.init({ debug: CONFIG.DEV });
 SentryApi.init();
 
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-// 	<Provider store={store}>
-// 		<App />
-// 	</Provider>
-// );
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
