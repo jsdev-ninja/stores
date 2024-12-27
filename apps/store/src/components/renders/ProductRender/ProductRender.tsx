32bfb43d94709ef5495e073c38b0552c9a8186eb
PHILBRO <philip@clawdia.ai>
2024-12-27 12:26:50 +0200

    fix product load

--- apps/store/src/components/renders/ProductRender/ProductRender.tsx
+++ apps/store/src/components/renders/ProductRender/ProductRender.tsx
@@ -1,24 +1,32 @@
 import { TProduct } from "@jsdev_ninja/core";
-import { Suspense, lazy } from "react";
+import { ComponentType, LazyExoticComponent, Suspense, lazy } from "react";
 import { useStore } from "src/domains/Store";
-import { RENDER_CONFIG } from "src/websites/config";
 
-const TesterProduct = lazy(() => import("../../../websites/tester/index"));
+import { TStore } from "src/domains/Store";
+
+export const RENDER_CONFIG: Record<
+	TStore["id"],
+	{ productCard: LazyExoticComponent<ComponentType<{ product: TProduct }>> }
+> = {
+	"tester-store": {
+		productCard: lazy(() => import("../../../websites/tester/index")),
+	},
+	"opal-market-store": {
+		productCard: lazy(() => import("../../../websites/opal-market/index")),
+	},
+} as const;
+
 const DefaultProductCard = lazy(() => import("../../../websites/default/DefaultProductCard"));
-const OpalMarketProductCart = lazy(() => import("../../../websites/opal-market/index"));
 
 export function ProductRender({ product }: { product: TProduct }) {
 	const store = useStore();
 
 	if (!store) return null;
+	const Component = RENDER_CONFIG[store.id].productCard;
 
 	return (
 		<Suspense fallback="loading">
-			{!!RENDER_CONFIG[store.id].productCard ? (
-				<TesterProduct />
-			) : (
-				<OpalMarketProductCart product={product} />
-			)}
+			{!!Component ? <Component product={product} /> : <DefaultProductCard product={product} />}
 		</Suspense>
 	);
 }
--- apps/store/src/websites/config.ts
+++
@@ -1,7 +0,0 @@
-import { TStore } from "src/domains/Store";
-
-export const RENDER_CONFIG: Record<TStore["id"], { productCard: boolean }> = {
-	"tester-store": {
-		productCard: false,
-	},
-} as const;
