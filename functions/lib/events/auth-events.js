c1342c3b1f0071346fb0d409cf0b27659eb9bc25
Philip <122067622+jsdev-ninja@users.noreply.github.com>
2025-02-15 11:51:06 +0200

    Delete functions/lib directory

--- functions/lib/events/auth-events.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// import admin from "firebase-admin";
-// import * as functions from "firebase-functions/v1";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=auth-events.js.map
