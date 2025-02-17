a2f2258457b33a729ab6e5eaac582c6644bfc54a
PHILBRO <philip@clawdia.ai>
2025-02-17 19:34:23 +0200

    create new store

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,5 +1,6 @@
 import { input, select } from "@inquirer/prompts";
 import { admin } from "./admin";
+import { TCompany, TStore } from "@jsdev_ninja/core";
 
 const stores = {
 	tester: {
@@ -11,7 +12,11 @@ const stores = {
 async function main() {
 	const action = await select({
 		message: "selection action",
-		choices: [{ value: "createAdmin", name: "create admin" }],
+		choices: [
+			{ value: "createAdmin", name: "create admin" },
+			{ value: "createCompany", name: "create company" },
+			{ value: "createStore", name: "create store" },
+		],
 	});
 
 	if (action === "createAdmin") {
@@ -40,6 +45,65 @@ async function main() {
 		};
 		await auth.setCustomUserClaims(userRecord.uid, token);
 		console.log("SUCCESS", action, selectedStoreName, email, password, name);
+	}
+
+	if (action === "createStore") {
+		const store: TStore = {
+			id: "",
+			companyId: "",
+			hypData: {
+				isJ5: "True",
+				KEY: "81057eb786ffc379de89d860031e8fea0e4d28f2",
+				masof: "0010302921",
+				password: "hyp1234",
+			},
+			name: "",
+			logoUrl: "",
+			tenantId: "",
+			urls: [],
+		};
+
+		const id = await input({ message: "enter store id" });
+		const name = await input({ message: "enter store name" });
+		const companyId = await input({ message: "enter  store  companyId" });
+		const tenantId = await input({ message: "enter store tenantId" });
+		const url = await input({ message: "enter store url" });
+
+		store.id = id;
+		store.name = name;
+		store.companyId = companyId;
+		store.tenantId = tenantId;
+		store.urls.push(url);
+
+		await admin.firestore().collection("stores").doc(id).set(store);
+
+		console.log("store", store);
+	}
+
+	if (action === "createCompany") {
+		const company: TCompany = {
+			id: "",
+			name: "",
+			websiteDomains: [],
+			owner: {
+				emails: { mainEmail: "" },
+				name: "",
+			},
+		};
+
+		const id = await input({ message: "enter company id" });
+		const name = await input({ message: "enter company name" });
+		const ownerName = await input({ message: "enter  owner name" });
+		const ownerEmil = await input({ message: "enter owner email" });
+
+		company.id = id;
+		company.name = name;
+		company.owner.name = ownerName;
+		company.owner.emails.mainEmail = ownerEmil;
+
+		await admin.firestore().collection("companies").doc(id).set(company);
+
+		console.log("company", company);
 	}
 }
 
