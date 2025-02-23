a4dce8f927316d2f6de4c78b42bf9d73c5801dbe
PHILBRO <philip@clawdia.ai>
2025-02-23 21:54:14 +0200

    clean

--- apps/store/src/infra/store/hooks.ts
+++ apps/store/src/infra/store/hooks.ts
@@ -31,13 +31,3 @@ export const useStoreActions = () => {
 
 	return result;
 };
-
-export const useFullID = () => {
-	const company = useAppSelector((state) => state.company);
-	const store = useAppSelector((state) => state.store);
-	const user = useAppSelector((state) => state.user);
-
-	if (!company.data?.id || !store.data?.id || !user.user?.uid) return "";
-
-	return `${company.data?.id}_${store.data?.id}_${user.user?.uid}`;
-};
