cc6810895afdf7b51755a0eabf69e87fd3e99a12
PHILBRO <philip@clawdia.ai>
2025-06-30 06:04:57 +0300

    fix

--- apps/store/tailwind.config.js
+++ apps/store/tailwind.config.js
@@ -6,8 +6,16 @@ export default {
 		"./index.html",
 		"./src/**/*.{js,ts,jsx,tsx}",
 		"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+		"../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
 			colors: {
 				primary: {
