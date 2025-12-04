8a1c1d38b7155a8ad5ee4712051508b2d99f619c
PHILBRO <philip@clawdia.ai>
2025-12-04 13:56:45 +0200

    update

--- packages/core/lib/entities/Payment/index.ts
+++ packages/core/lib/entities/Payment/index.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 // payment type priority -> store -> organization -> profile (profile is stronger)
 // value fixed on order creation, can be changed later by admin
 
-export const PaymentTypeSchema = z.enum(["external", "j5"]);
+export const PaymentTypeSchema = z.enum(["external", "j5", "none"]);
 
 export type TPaymentType = z.infer<typeof PaymentTypeSchema>;
