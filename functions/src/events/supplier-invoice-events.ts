737f657545f415d69c8796917d7f23298c1cdc6d
PHILBRO <philip@clawdia.ai>
2025-12-25 15:14:06 +0200

    fix

--- functions/src/events/supplier-invoice-events.ts
+++ functions/src/events/supplier-invoice-events.ts
@@ -2,10 +2,11 @@ import { FirebaseAPI } from "@jsdev_ninja/core";
 import admin from "firebase-admin";
 import * as functions from "firebase-functions/v2";
 
-export const onSupplierInvoiceCreate = functions.firestore.onDocumentCreated(
-	FirebaseAPI.firestore.getPath({ collectionName: "supplierInvoices" }),
-	async (snap, context) => {
-		const supplierInvoice = snap.data() as TSupplierInvoice;
-		console.log("supplier invoice create", supplierInvoice);
-	}
-);
+// export const onSupplierInvoiceCreate = functions.firestore.onDocumentCreated(
+// 	FirebaseAPI.firestore.getPath({ collectionName: "supplierInvoices" }),
+// 	async (snap, context) => {
+// 		const supplierInvoice = snap.data() as TSupplierInvoice;
+// 		console.log("supplier invoice create", supplierInvoice);
+// 	}
+// );
+export {};
