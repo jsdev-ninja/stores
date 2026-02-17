0cbdca6f2aa9385f8634418b7e4d52fd31f38a78
PHILBRO <philip@clawdia.ai>
2026-02-18 03:15:29 +0800

    testing chat

--- apps/store/src/features/chatbot/index.tsx
+++ apps/store/src/features/chatbot/index.tsx
@@ -1,5 +1,6 @@
 import { ChatbotProvider } from "./context";
 import { ChatbotToggle } from "./ChatbotToggle";
+import { useUser } from "src/domains/user";
 
 export { ChatbotProvider, useChatbot } from "./context";
 export { ChatbotToggle } from "./ChatbotToggle";
@@ -8,8 +9,9 @@ export { ChatMessages } from "./ChatMessages";
 export { ChatInput } from "./ChatInput";
 
 export function ChatbotFeature() {
+	const user = useUser();
 	return (
-		<ChatbotProvider isFeatureOn={false}>
+		<ChatbotProvider isFeatureOn={user?.email === "balasistore6@gmail.com"}>
 			<ChatbotToggle />
 		</ChatbotProvider>
 	);
